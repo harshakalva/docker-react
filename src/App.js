23159e80ffba497104a9d83181988e561df4441e
Harsha Kalva <kalvhar@hilti.com>
2020-03-29 18:19:57 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There  ! Updated
+          I was changed in feature branch
         </p>
         <a
           className="App-link"
